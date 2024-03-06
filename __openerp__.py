57396c802117a15f66b223a6114d49648412feeb
zz <zhuzi813@gmail.com>
2024-03-06 17:40:16 +0100

    odoo8 product ean uniq

---
+++ __openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Product Unique EAN Code',
+    'version': '8.0.1.0.0',
+    'category': 'Product',
+    'description': """
+Module to made "ean13" fields to be unique for products.
+===========================================================
+""",
+    'author': 'zz',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': ['base', 'product'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
